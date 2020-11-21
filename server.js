6cb5cc405a04af2c40c0f26ed41df5d469593e53
tony913 <tonycrosby96@gmail.com>
2020-11-21 16:23:25 -0600

    adding the express with the public static.

--- server.js
+++ server.js
@@ -2,4 +2,13 @@
 const express = require("express");
 const path = require("path");
 const fs = require("fs");
-const db = require("./db/db.json")
+const db = require("./db/db.json");
+
+//Express app
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+//Express set up
+app.use(express.urlencoded({ extended:  true}));
+app.use(express.json());
+app.use(express.static("public"));
