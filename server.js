2212b1432dafe6a6fe7cdf8ee6cf088e9a69de14
tony913 <tonycrosby96@gmail.com>
2020-11-21 16:35:53 -0600

    updating server.js

--- server.js
+++ server.js
@@ -14,20 +14,36 @@ app.use(express.json());
 app.use(express.static("public"));
 
 // get API notes-sends user to AJAX page
-app.get("/", function (req, res) {
+app.get("/", (req, res) => {
     res.sendFile(path.join(__dirname, "./public/notes.html"));
 });
 
-app.get("/notes", function(req, res) {
-    res.sendFile(path.join(_dirname, "./public/notes.html"));
+app.get("/notes", (req, res) => {
+    res.sendFile(path.join(__dirname, "./public/notes.html"));
 });
 
 // displays all notes
-app.get("/api/notes", function(req, res) {
+app.get("/api/notes", (req, res) => {
     return res.json(db);
 });
 
+app.post("api/notes", (req, res) => {
+    let newNote = req.body;
+    newNote["id"] = db.length;
+    console.log(newNote);
+    db.push(newNote);
+});
+fs.writeFile("./public/db/db.json", JSON.stringify(db), (_, err) => {
+    if (err) throw err;
+    console.log("Noted");
+});
 
+// delete notes
+app.delete("/api/notes/:id", (req,res) => {
+    const chosenNote = req.params.id;
+    console.log(chosenNote);
+    console.log(db);
+});
 
 //server listening
 app.listen(PORT, function() {
