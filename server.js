6273bb14185acecd2030caa4e775c3f5b11e13a3
tony913 <tonycrosby96@gmail.com>
2020-11-21 16:25:33 -0600

    adding the gets

--- server.js
+++ server.js
@@ -4,11 +4,21 @@ const path = require("path");
 const fs = require("fs");
 const db = require("./db/db.json");
 
-//Express app
+// Express app
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-//Express set up
+// Express set up
 app.use(express.urlencoded({ extended:  true}));
 app.use(express.json());
 app.use(express.static("public"));
+
+// get API notes-sends user to AJAX page
+app.get("/", function (req, res) {
+    res.sendFile(path.join(__dirname, "./public/notes.html"));
+});
+
+app.get("/notes", function(req, res) {
+    res.sendFile(path.join(_dirname, "./public/notes.html"));
+});
+
